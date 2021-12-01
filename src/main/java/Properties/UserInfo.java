652992687566b2257cbb4bd6e42f057ea6e7deef
UnalDonmez <79373213+UnalDonmez@users.noreply.github.com>
2021-12-01 15:48:07 +0300

    Unal

--- src/main/java/Properties/UserInfo.java
+++ src/main/java/Properties/UserInfo.java
@@ -1,6 +1,6 @@
 package Properties;
 
 public class UserInfo {
-    public static String email = "unal_donmez1903@hotmail.com";
-    public static String password = "Lunal2021";
+    public static String email = "una";
+    public static String password = "şifre";
 }
